b13d34ed3ebcd4437d6e4068fb5271e5a66f37ed
jbuisine <jerome.buisine@outlook.fr>
2018-07-16 06:08:03 +0200

    First implementation

--- mainExample.cpp
+++ mainExample.cpp
@@ -1,3 +1,26 @@
 //
 // Created by Jérôme BUISINE on 16/07/2018.
 //
+
+#include <iostream>
+#include <TreeNodeExample.h>
+
+int main () {
+
+    unsigned numberOfIterations = 100;
+
+    TreeNodeExample* rootNode = new TreeNodeExample(0);
+
+    for(unsigned i = 0; i < numberOfIterations; i++){
+        rootNode->selectAction();
+    }
+
+    // TODO : check link issue
+    TreeNode* nextNodeToUse = rootNode->getBestChild();
+    unsigned nodeIndex = nextNodeToUse->getIndex();
+
+    delete rootNode;
+
+    std::cout << "Next node to use is [Node " << nextNodeToUse << "]" << std::endl;
+
+}
---
+++ mcts/TreeNodeExample.h
@@ -0,0 +1,32 @@
+#pragma once
+
+// MCTS
+#include "TreeNode.h"
+#include <vector>
+
+class TreeNodeExample : public TreeNode
+{
+public:
+	
+	// summary : Constructor of TreeNodeExample
+	// parameter : @_index is the index of current node
+    TreeNodeExample(unsigned _index);
+
+	// summary : Step 2 (expansion) : open new nodes (if current node is not terminal node)
+	// returns : nothing
+	virtual void expand();
+
+	// summary : Step 3 (simulation) : Get roll out of current node chosen
+	// parameter : @_node is the node selected to get roll out
+	// returns : double
+	double rollOut(TreeNode* _node);
+
+	// summary : Returns current best tree node
+	// returns : TreeNode*
+	TreeNode* getBestChild();
+
+private:
+
+	// Here implement your own needs such like context or information to keep in node
+};
+
